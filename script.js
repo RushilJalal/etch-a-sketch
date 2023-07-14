82967375f14e3cee38e03573277121af0cf30001
Rushil Jalal <rushiljalal@gmail.com>
2023-07-14 12:12:05 +0530

    Add trail effect on mouse hover

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const div = document.querySelector("div");
 function makeGrid(rows, cols) {
     for (i = 0; i < rows * cols; i++) {
         const cell = document.createElement("div");
+        // cell.textContent = i+1;
         cell.classList.add("cell");
-        // cell.innerText = i+1;
         container.appendChild(cell);
     }
 }
 
-makeGrid(16,16);
+makeGrid(16, 16);
+
+const squares = document.querySelectorAll(".cell");
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.classList.add("hover")
+    });
+});

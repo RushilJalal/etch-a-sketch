f3ff0c178f333c8757b25985e34f5c8ff2afc3c9
Rushil Jalal <rushiljalal@gmail.com>
2023-07-15 13:39:48 +0530

    Add Change Grid button

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const container = document.querySelector(".container");
 const div = document.querySelector("div");
 
-function makeGrid(rows, cols) {
-    for (i = 0; i < rows * cols; i++) {
+const defaultLen = 16;
+function makeGrid(length) {
+    for (let i = 0; i < length * length; i++) {
         const cell = document.createElement("div");
-        // cell.textContent = i+1;
         cell.classList.add("cell");
+        cell.style.height = `calc(100% / ${length})`;
+        cell.style.width = `calc(100% / ${length})`;
         container.appendChild(cell);
     }
+    draw();
 }
 
-makeGrid(16, 16);
+makeGrid(defaultLen);
 
-const squares = document.querySelectorAll(".cell");
-squares.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("hover")
+function draw() {
+    const squares = document.querySelectorAll(".cell");
+    squares.forEach(square => {
+        square.addEventListener("click", () => square.classList.add("hover"));
+        square.addEventListener("mouseover", (e) => {
+            if (e.buttons === 1) { //left click drag
+                square.classList.add("hover");
+            }
+        });
     });
-});
+}
+
+const changeGrid = document.querySelector(".btn");
+changeGrid.addEventListener("click", () => {
+    new_length = prompt("Enter new grid size");
+    while (container.firstChild)
+        container.removeChild(container.firstChild);
+    makeGrid(new_length);
+})

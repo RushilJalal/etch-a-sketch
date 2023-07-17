1f06bf5c6384b6f0905cd7c6a4b40891baed8f1f
Rushil Jalal <rushiljalal@gmail.com>
2023-07-17 22:26:41 +0530

    Add color picker

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const div = document.querySelector("div");
 
-const defaultLen = 16;
+const defaultLen = 24;
 function makeGrid(length) {
     for (let i = 0; i < length * length; i++) {
         const cell = document.createElement("div");
@@ -18,19 +18,29 @@ makeGrid(defaultLen);
 function draw() {
     const squares = document.querySelectorAll(".cell");
     squares.forEach(square => {
-        square.addEventListener("click", () => square.classList.add("hover"));
+        square.addEventListener("click", () => {
+            square.style.backgroundColor = myPenColor;
+        })
         square.addEventListener("mouseover", (e) => {
             if (e.buttons === 1) { //left click drag
-                square.classList.add("hover");
+                square.style.backgroundColor = myPenColor;
             }
         });
     });
 }
 
-const changeGrid = document.querySelector(".btn");
+const changeGrid = document.querySelector("#change-grid");
 changeGrid.addEventListener("click", () => {
     new_length = prompt("Enter new grid size");
     while (container.firstChild)
         container.removeChild(container.firstChild);
     makeGrid(new_length);
 })
+
+let myPenColor = "black";
+const colorPicker = document.querySelector("#color-picker");
+
+colorPicker.addEventListener("change", (e) => {
+    myPenColor = e.target.value;
+});
+colorPicker.select();
